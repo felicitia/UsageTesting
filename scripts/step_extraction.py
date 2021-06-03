bd2c49caab9e79091a50524fc52cfb21360d7b21
Leon <43097991+unkn-wn@users.noreply.github.com>
2021-06-03 12:41:37 -0700

    2nd version

--- scripts/step_extraction.py
+++ scripts/step_extraction.py
@@ -1,30 +1,38 @@
 import json
-import shutil
+import shutil, os
 
 frame_index = 0 #Index of the frame list
-dir_to_json = 'detected_actions.json' #directory to json file
-dir_to_frames = '/Users/jasonyee/Downloads/ACT-Mac-App/step_extraction/detected_frames/' #directory to the folder with frames
-dir_to_new_folder = '/Users/jasonyee/Downloads/ACT-Mac-App/step_extraction/detected_frames/clicked_frames/' #directory to a new folder for the clicked frames
+dir_to_videos = "/Users/jasonyee/Downloads/ACT-Mac-App/step_extraction/video_data" #directory to video data
 
-file = open(dir_to_json,)
-data = json.load(file)
 
-for j in range(len(data)):
-    img_index = (data[j]['frames'][frame_index])
-    
-    if(len(str(img_index)) == 1):
-        img = 'bbox-000'+str(data[j]['frames'][frame_index])+'.jpg'
-    elif(len(str(img_index)) == 2):
-        img = 'bbox-00'+str(data[j]['frames'][frame_index])+'.jpg'
-    elif(len(str(img_index)) == 3):
-        img = 'bbox-0'+str(data[j]['frames'][frame_index])+'.jpg'
-    elif(len(str(img_index)) == 4):
-        img = 'bbox-'+str(data[j]['frames'][frame_index])+'.jpg'
+for folder in os.listdir(dir_to_videos):
+    dir_to_json = dir_to_videos+"/"+folder+"/detected_actions.json"
+    dir_to_frames = dir_to_videos+"/"+folder+"/detected_frames"
+    dir_to_new_folder = dir_to_videos+"/"+folder+"/clicked_frames"
 
-    imgdir = dir_to_frames+img
-    print(img)
-    
-    shutil.move(imgdir, dir_to_new_folder+img)
-    
+    if not os.path.exists(dir_to_new_folder):
+        os.makedirs(dir_to_new_folder)
 
-file.close()
+    print("\n\n"+folder+"\n")
+    file = open(dir_to_json,)
+    data = json.load(file)
+
+    for j in range(len(data)):
+        img_index = (data[j]['frames'][frame_index])
+        
+        if(len(str(img_index)) == 1):
+            img = '/bbox-000'+str(data[j]['frames'][frame_index])+'.jpg'
+        elif(len(str(img_index)) == 2):
+            img = '/bbox-00'+str(data[j]['frames'][frame_index])+'.jpg'
+        elif(len(str(img_index)) == 3):
+            img = '/bbox-0'+str(data[j]['frames'][frame_index])+'.jpg'
+        elif(len(str(img_index)) == 4):
+            img = '/bbox-'+str(data[j]['frames'][frame_index])+'.jpg'
+
+        imgdir = dir_to_frames+img
+        print(img)
+        
+        shutil.move(imgdir, dir_to_new_folder+img)
+        
+
+    file.close()
