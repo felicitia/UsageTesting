8900c61656efc9c1a35da891c9fdac4edf273c40
Leon <43097991+unkn-wn@users.noreply.github.com>
2021-06-03 12:45:43 -0700

    2nd version

--- scripts/step_extraction.py
+++ scripts/step_extraction.py
@@ -2,13 +2,13 @@ import json
 import shutil, os
 
 frame_index = 0 #Index of the frame list
-dir_to_videos = "/Users/jasonyee/Downloads/ACT-Mac-App/step_extraction/video_data" #directory to video data
+dir_to_videos = "/Users/jasonyee/Downloads/ACT-Mac-App/step_extraction/video_data/" #directory to video data
 
 
 for folder in os.listdir(dir_to_videos):
-    dir_to_json = dir_to_videos+"/"+folder+"/detected_actions.json"
-    dir_to_frames = dir_to_videos+"/"+folder+"/detected_frames"
-    dir_to_new_folder = dir_to_videos+"/"+folder+"/clicked_frames"
+    dir_to_json = dir_to_videos+folder+"/detected_actions.json"
+    dir_to_frames = dir_to_videos+folder+"/detected_frames/"
+    dir_to_new_folder = dir_to_videos+folder+"/clicked_frames/"
 
     if not os.path.exists(dir_to_new_folder):
         os.makedirs(dir_to_new_folder)
@@ -21,13 +21,13 @@ for folder in os.listdir(dir_to_videos):
         img_index = (data[j]['frames'][frame_index])
         
         if(len(str(img_index)) == 1):
-            img = '/bbox-000'+str(data[j]['frames'][frame_index])+'.jpg'
+            img = 'bbox-000'+str(data[j]['frames'][frame_index])+'.jpg'
         elif(len(str(img_index)) == 2):
-            img = '/bbox-00'+str(data[j]['frames'][frame_index])+'.jpg'
+            img = 'bbox-00'+str(data[j]['frames'][frame_index])+'.jpg'
         elif(len(str(img_index)) == 3):
-            img = '/bbox-0'+str(data[j]['frames'][frame_index])+'.jpg'
+            img = 'bbox-0'+str(data[j]['frames'][frame_index])+'.jpg'
         elif(len(str(img_index)) == 4):
-            img = '/bbox-'+str(data[j]['frames'][frame_index])+'.jpg'
+            img = 'bbox-'+str(data[j]['frames'][frame_index])+'.jpg'
 
         imgdir = dir_to_frames+img
         print(img)
